ed8cfa6c17705a9a34c104454a4ec73fd95fee5a
Jordan Hall <jordan@libertyware.co.uk>
2024-10-28 21:58:37 +0000

    899f711556 feat(compiler): selectorless components for JIT feat(compiler): Make selector optional and name for pipes

--- static/index.d.ts
+++ static/index.d.ts
@@ -1,5 +1,5 @@
 /**
- * @license Angular v19.1.0-next.0+sha-dca1483-with-local-changes
+ * @license Angular v19.1.0-next.0+sha-899f711-with-local-changes
  * (c) 2010-2024 Google LLC. https://angular.io/
  * License: MIT
  */
